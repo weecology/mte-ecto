373ecfe24661a5069adeb218dcca735dbcbb6a39
Morgan Ernest <morgan@weecology.org>
2013-02-06 09:14:53 -0700

    Initial Commit of main file
    The main file currently imports the MTE_params library to create the
    metabolic equation parameters.

---
+++ MTE_Ecto_main.py
@@ -0,0 +1,6 @@
+from __future__ import division
+
+import MTE_params
+
+metabolic_params = MTE_params.get_class_MTE_params("Class_metabolicrates_Makrievadata.csv", 
+                            "gillooly_fish.csv", "Whiteetal_Amphibiandata.csv")
