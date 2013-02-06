189e4cc9c53839cdc904fa19c01172bc1b36a322
Morgan Ernest <morgan@weecology.org>
2013-02-06 10:31:02 -0700

    linked unique replicate ID w/ species names
    Each study has a unique ID (replicate). Made function that links this ID
    with the name of the species used in that study.

--- MTE_Ecto_main.py
+++ MTE_Ecto_main.py
@@ -1,6 +1,31 @@
 from __future__ import division
 
+import pandas as pd
+
 import MTE_params
 
+def create_species_replicateID_linker(data):
+    replicates = list(set(data['replicate']))
+    replicate_species_linker = []
+    
+    for replicate in replicates:
+        replicate_data = data.ix[data['replicate'] == replicate]
+        replicate_species = list(set(replicate_data['Species']))
+        replicate_class = list(set(replicate_data['Class']))
+        replicate_info = [replicate, replicate_species, replicate_class]
+        replicate_species_linker.append(replicate_info)  
+    return replicate_species_linker
+
+"""Main Code"""
 metabolic_params = MTE_params.get_class_MTE_params("Class_metabolicrates_Makrievadata.csv", 
-                            "gillooly_fish.csv", "Whiteetal_Amphibiandata.csv")
+                                                   "gillooly_fish.csv", 
+                                                   "Whiteetal_Amphibiandata.csv")
+
+SizeTemp_data = pd.read_csv("MTEEcto_data.csv")
+replicate_species_list = create_species_replicateID_linker(SizeTemp_data)
+
+
+    
+    
+
+
