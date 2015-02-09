00f9765b6e0a9be39571c69c61aa4d23f923e3a6
Kristina Riemer <reamy316@aim.com>
2015-02-09 14:54:55 -0500

    Continue exploratory stats and figs on met rate and mass values

--- MetabolicRate.R
+++ MetabolicRate.R
@@ -128,6 +128,9 @@ three_degree_pairs = cbind(three_degree_pairs, store_class_values$exponent, stor
 colnames(three_degree_pairs)[11] = "exponent"
 colnames(three_degree_pairs)[12] = "Ea"
 
+# List of classes and number of pairs per class
+class_distribution = table(three_degree_pairs$Class)
+
 #------------------CALCULATE METABOLIC RATES/MASSES--------------------------
 
 # Use MTE equation to calculate metabolic rates (initial, final, and w/ constant/initial mass)
@@ -138,9 +141,6 @@ three_degree_pairs$constantmass_metrate = (three_degree_pairs$initial_mass ^ thr
 # Use rearranged MTE equation to calculate mass w/ constant/initial metabolic rate
 three_degree_pairs$constantmetrate_mass = (three_degree_pairs$initial_metrate / (exp(three_degree_pairs$Ea / (.00008617 * (three_degree_pairs$final_temp + 273.15))))) ^ (1 / three_degree_pairs$exponent)
 
-# Arrange dataset according to class
-three_degree_pairs = three_degree_pairs[with(three_degree_pairs, order(Class)), ]
-
 #--------------------EXPLORATORY STATS AND FIGURES-----------------------
 
 ### Summary values
@@ -149,7 +149,6 @@ three_degree_pairs = three_degree_pairs[with(three_degree_pairs, order(Class)), 
   # 1: initial to final (empirical), expect increase
   # 2: initial to constant mass (theoretical), expect increase
   # 3: final to constant mass, expect increase
-
 percent_diff1 = ((three_degree_pairs$final_metrate / three_degree_pairs$initial_metrate) - 1) * 100
 percent_diff2 = ((three_degree_pairs$constantmass_metrate / three_degree_pairs$initial_metrate) - 1) * 100
 percent_diff3 = ((three_degree_pairs$constantmass_metrate / three_degree_pairs$final_metrate) - 1) * 100
@@ -158,34 +157,115 @@ percent_diff3 = ((three_degree_pairs$constantmass_metrate / three_degree_pairs$f
   # 4: initial to final (empirical), expect decrease
   # 5: initial to constant metabolic rate (theoretical), expect decrease
   # 6: final to constant metabolic rate, expect decrease
-
 percent_diff4 = ((three_degree_pairs$final_mass / three_degree_pairs$initial_mass) - 1) * 100
 percent_diff5 = ((three_degree_pairs$constantmetrate_mass / three_degree_pairs$initial_mass) - 1) * 100
 percent_diff6 = ((three_degree_pairs$constantmetrate_mass / three_degree_pairs$final_mass) - 1) * 100
 
+# Average percent differences
+avg_pd_1 = mean(percent_diff1)
+avg_pd_2 = mean(percent_diff2)
+avg_pd_3 = mean(percent_diff3)
+avg_pd_4 = mean(percent_diff4)
+avg_pd_5 = mean(percent_diff5)
+avg_pd_6 = mean(percent_diff6)
+
+# Standard deviations of percent differences
+sd_pd_1 = sd(percent_diff1)
+sd_pd_2 = sd(percent_diff2)
+sd_pd_3 = sd(percent_diff3)
+sd_pd_4 = sd(percent_diff4)
+sd_pd_5 = sd(percent_diff5)
+sd_pd_6 = sd(percent_diff6)
+
 ### Statistical tests
 
+# T-tests for metabolic rates
+  # 1: initial to final (empirical)
+  # 2: initial to constant mass (theoretical)
+  # 3: final to constant mass
+ttest1 = t.test(three_degree_pairs$initial_metrate, three_degree_pairs$final_metrate, paired = TRUE)
+ttest2 = t.test(three_degree_pairs$initial_metrate, three_degree_pairs$constantmass_metrate, paired = TRUE)
+ttest3 = t.test(three_degree_pairs$final_metrate, three_degree_pairs$constantmass_metrate, paired = TRUE)
+
+# T-tests for masses
+  # 4: initial to final (empirical)
+  # 5: initial to constant metabolic rate (theoretical)
+  # 6: final to constant metabolic rate
+ttest4 = t.test(three_degree_pairs$initial_mass, three_degree_pairs$final_mass, paired = TRUE)
+ttest5 = t.test(three_degree_pairs$initial_mass, three_degree_pairs$constantmetrate_mass, paired = TRUE)
+ttest6 = t.test(three_degree_pairs$final_mass, three_degree_pairs$constantmetrate_mass, paired = TRUE)
 
 ### Figures
 
-# Histogram of all metabolic rates
-hist(log(three_degree_pairs$initial_metrate))
-hist(log(three_degree_pairs$final_metrate), col = rgb(0, 0, 1, 0.5), add = T)
-hist(log(three_degree_pairs$constantmass_metrate), col = rgb(1, 0, 0, 0.5), add = T)
+# Histograms not useful
+# # Histogram of all metabolic rates
+# hist(log(three_degree_pairs$initial_metrate))
+# hist(log(three_degree_pairs$final_metrate), col = rgb(0, 0, 1, 0.5), add = T)
+# hist(log(three_degree_pairs$constantmass_metrate), col = rgb(1, 0, 0, 0.5), add = T)
+# 
+# # Histogram of all mass values
+# hist(log(three_degree_pairs$initial_mass))
+# hist(log(three_degree_pairs$final_mass), col = rgb(0, 0, 1, 0.5), add = T)
+# hist(log(three_degree_pairs$constantmetrate_mass), col = rgb(1, 0, 0, 0.5), add = T)
 
-# Histogram of all mass values
-hist(log(three_degree_pairs$initial_mass))
-hist(log(three_degree_pairs$final_mass), col = rgb(0, 0, 1, 0.5), add = T)
-hist(log(three_degree_pairs$constantmetrate_mass), col = rgb(1, 0, 0, 0.5), add = T)
+# Density plot of metabolic rates, log-transformed
+plot(density(log(three_degree_pairs$initial_metrate)), col = "orange", main = "", 
+     xlab = "log(metabolic rate value)", ylab = "metabolic rate density")
+lines(density(log(three_degree_pairs$final_metrate)), col = "green")
+lines(density(log(three_degree_pairs$constantmass_metrate)), col = "purple")
+legend("topright", c("initial", "final", "constant mass"), title = "Metabolic rates:",
+       fill = c("orange", "green", "purple"))
+
+# Density plot of mass values, log-transformed
+plot(density(log(three_degree_pairs$initial_mass)), col = "orange", main = "", 
+     xlab = "log(mass value)", ylab = "mass density")
+lines(density(log(three_degree_pairs$final_mass)), col = "green")
+lines(density(log(three_degree_pairs$constantmetrate_mass)), col = "purple")
+legend("topright", c("initial", "final", "constant met rate"), title = "Masses:",
+       fill = c("orange", "green", "purple"))
+
+# 1:1 plots not useful, though mass values one is slightly clearer
+# # 1:1 plot of metabolic rates
+# plot(log(three_degree_pairs$initial_metrate), log(three_degree_pairs$final_metrate), col = "green")
+# points(log(three_degree_pairs$initial_metrate), log(three_degree_pairs$constantmass_metrate), col = "purple")
+# lines(x = c(-40, 0), y = c(-40, 0))
+# 
+# # 1:1 plot of mass values
+# plot(log(three_degree_pairs$initial_mass), log(three_degree_pairs$final_mass), col = "green")
+# points(log(three_degree_pairs$initial_mass), log(three_degree_pairs$constantmetrate_mass), col = "purple")
+# lines(x = c(-7, 7), y = c(-7, 7))
 
 # Barplot of percent differences for metabolic rates; expect 0 < diff3 < diff1 < diff2
-barplot(percent_diff2, col = rgb(0, 0, 1, 0.5))
+barplot(percent_diff2, col = rgb(0, 0, 1, 0.5), ylab = "Metabolic rate percent diff")
 barplot(percent_diff1, add = T)
 barplot(percent_diff3, col = rgb(1, 0, 0, 0.5), add = T)
+legend("topright", c("actual diff (1)", "no mass change diff (2)", "(3)"), cex = 0.8, 
+       title = "Met rate comparison", fill = c("white", "blue", "red"))
 
-# Barplot of percent differences for masses
-barplot(percent_diff5, col = rgb(0, 0, 1, 0.5))
+# Barplot of percent differences for masses; expect 0 > diff4 > diff 5, uncertain diff6
+barplot(percent_diff5, col = rgb(0, 0, 1, 0.5), ylab = "Mass percent diff")
+barplot(percent_diff6, col = rgb(1, 0, 0, 0.5), add = T)
 barplot(percent_diff4, add = T)
-barplot(percent_diff6, col = rgb(1, 0, 0, 0.5))
+legend("topright", c("actual diff (4)", "needed diff (5)", "(6)"), cex = 0.8, title = "Mass comparison", 
+       fill = c("white", "blue", "red"))
+
+# Barplot of average percent difference for metabolic rates
+# Possibly add SD in future
+barplot(c(avg_pd_1, avg_pd_2, avg_pd_3), col = c("white", "blue", "red"), 
+        names.arg = c("actual diff (1)", "no mass change diff (2)", "(3)"), 
+        xlab = "Met rate comparison", ylab = "Avg percent diff")
+
+# Barplot of average percent difference for masses
+# Possibly add SD in future
+barplot(c(avg_pd_4, avg_pd_5, avg_pd_6), col = c("white", "blue", "red"), 
+        names.arg = c("actual diff (4)", "needed diff (5)", "(6)"), 
+        xlab = "Mass comparison", ylab = "Avg percent diff")
 
 #-----------------------------ABSOLUTE TEMP-------------------------------
+
+# Arrange dataset according to class
+#three_degree_pairs = three_degree_pairs[with(three_degree_pairs, order(Class)), ]
+
+# Arrange dataset according to initial temperature
+#three_degree_pairs = three_degree_pairs[with(three_degree_pairs, order(initial_temp)), ]
+
