8b4656ca58066e4359a637021bfa800e53e62564
Kristina Riemer <kristinariemer@Kristinas-MacBook-Pro.local>
2015-09-24 12:31:15 -0400

    Add transformed values and initial LMM

--- MetabolicRate.R
+++ MetabolicRate.R
@@ -141,7 +141,57 @@ three_degree_pairs$constantmass_metrate = (three_degree_pairs$initial_mass ^ thr
 # Use rearranged MTE equation to calculate mass w/ constant/initial metabolic rate
 three_degree_pairs$constantmetrate_mass = (three_degree_pairs$initial_metrate / (exp(three_degree_pairs$Ea / (.00008617 * (three_degree_pairs$final_temp + 273.15))))) ^ (1 / three_degree_pairs$exponent)
 
+#----------------CALCULATE LOG TRANSFORMED VALUES-----------------------------
+
+# Log transform all metabolic rate & mass values
+three_degree_pairs$log_initial_mass = log(three_degree_pairs$initial_mass)
+three_degree_pairs$log_initial_metrate = log(three_degree_pairs$initial_metrate)
+three_degree_pairs$log_final_mass = log(three_degree_pairs$final_temp)
+three_degree_pairs$log_final_metrate = log(three_degree_pairs$final_metrate)
+three_degree_pairs$log_constantmass_metrate = log(three_degree_pairs$constantmass_metrate)
+three_degree_pairs$log_constantmetrate_mass = log(three_degree_pairs$constantmetrate_mass)
+
+# Histograms of transformed values
+hist(three_degree_pairs$log_initial_mass)
+hist(three_degree_pairs$log_initial_metrate)
+hist(three_degree_pairs$log_final_mass)
+hist(three_degree_pairs$log_final_metrate)
+hist(three_degree_pairs$log_constantmass_metrate)
+hist(three_degree_pairs$log_constantmetrate_mass)
+
+# Comparison of final and constant mass transformed metabolic rates
+three_degree_pairs$metrate_comparison = three_degree_pairs$log_constantmass_metrate - three_degree_pairs$log_final_metrate
+mean(three_degree_pairs$metrate_comparison)
+hist(three_degree_pairs$metrate_comparison)
+t.test(three_degree_pairs$log_final_metrate, three_degree_pairs$log_constantmass_metrate, paired = TRUE)
+boxplot(three_degree_pairs$log_final_metrate, three_degree_pairs$log_constantmass_metrate, main = "Mass change mediates MR increase", xlab = "Mass", ylab = "Log metabolic rate", names = c("actual (i.e., varying)", "constant"))
+
+# Comparison of final and constant MR transformed masses
+three_degree_pairs$mass_comparison = three_degree_pairs$log_constantmetrate_mass - three_degree_pairs$log_final_mass
+mean(three_degree_pairs$mass_comparison)
+hist(three_degree_pairs$mass_comparison)
+t.test(three_degree_pairs$log_final_mass, three_degree_pairs$log_constantmetrate_mass, paired = TRUE)
+boxplot(three_degree_pairs$log_final_mass, three_degree_pairs$log_constantmetrate_mass, main = "Required mass change to maintain MR", xlab = "Metabolic rate", ylab = "Log mass", names = c("actual", "constant (i.e., compensation mass)"))
+
 #--------------------EXPLORATORY STATS AND FIGURES-----------------------
+
+
+# ------------------Mixed model----------
+
+library(lme4)
+
+three_degree_pairs$releveled_class = relevel(factor(three_degree_pairs$Class), "Insecta")
+
+three_degree_pairs$paper_id = gsub("[A-Z]$", "", as.character(three_degree_pairs$studyID))
+
+summary(lmer(mass_comparison ~ (1|studyID), data = three_degree_pairs))
+summary(lmer(mass_comparison ~ (1|species), data = three_degree_pairs))
+summary(lmer(mass_comparison ~ releveled_class + (1|species), data = three_degree_pairs))
+
+summary(lmer(mass_comparison ~ releveled_class + (1|species) + (1|paper_id), data = three_degree_pairs))
+
+# Don't publish based on these numbers
+round(ranef(lmer(mass_comparison ~ releveled_class + (1|species), data = three_degree_pairs))[[1]], 3)
 
 ### Summary values
 
