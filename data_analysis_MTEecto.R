80770cbf0ba2bc9ae35c906926bf1fc2ec538de1
Kristina Riemer <kristina.riemer@weecology.org>
2016-06-08 17:05:31 -0400

    Exploring species' pairs, new plots

--- data_analysis_MTEecto.R
+++ data_analysis_MTEecto.R
@@ -1,25 +1,87 @@
 # Read in clean data
-replicates_data = read.csv("clean_data_MTEecto.csv", stringsAsFactors = FALSE)
+pairs_data = read.csv("clean_data_MTEecto.csv", stringsAsFactors = FALSE)
+
+#---------------------------PAIRS-------------------
+
+# Metabolic rate
+pairs_data$PD_MR_CS = ((pairs_data$constantmass_metrate / pairs_data$initial_metrate) - 1) * 100
+pairs_data$PD_MR_VS = ((pairs_data$final_metrate / pairs_data$initial_metrate) - 1) * 100
+pairs_data$MR_diff = pairs_data$PD_MR_CS - pairs_data$PD_MR_VS
+pairs_data$PD_mass = ((pairs_data$final_mass / pairs_data$initial_mass) - 1) * 100
+pairs_data = pairs_data[order(pairs_data$MR_diff),]
+barplot(pairs_data$PD_MR_VS, col = "green", xlab = "Pairs", ylab = "MR change due to temp increase")
+barplot(pairs_data$PD_MR_CS, col = rgb(0, 0, 0, alpha = 0.3), add = TRUE)
+legend("topright", c("constant mass/no STR", "varying mass/STR"), fill = c("white", "green"))
+
+#TODO: MR PDs for all pairs grouped by species
+#pairs_bp = pairs_data[pairs_data, order(MR_diff, species),]
+
 
 #---------------METABOLIC RATE ANALYSIS & VISUALIZATION----------
 
 ### Metabolic rate percent differences
-replicates_data$PD_MR_CS = ((replicates_data$constantmass_metrate / replicates_data$initial_metrate) - 1) * 100
-replicates_data$PD_MR_VS = ((replicates_data$final_metrate / replicates_data$initial_metrate) - 1) * 100
 
 library(dplyr)
-species_data = replicates_data %>% 
+species_data = pairs_data %>% 
   group_by(species) %>% 
   summarise_each(funs(mean, sd), PD_MR_CS, PD_MR_VS)
 
-occurrences = replicates_data %>% 
+occurrences = pairs_data %>% 
   group_by(species) %>% 
   summarise(count = n())
 species_data$occurrences = occurrences$count
 
 # Looking at variation in percent differences for each species w/ sufficient pairs
 species_data = species_data[order(species_data$occurrences, decreasing = TRUE),]
-sub_species_data = species_data[1:7,]
+sub_species_data = species_data[1:4,]
+
+par(mfrow = c(2, 2))
+sp1 = pairs_data[pairs_data$species == "Drosophila melanogaster  ",]
+sp1$PD_mass = ((sp1$final_mass / sp1$initial_mass) - 1) * 100
+sp1 = sp1[order(sp1$PD_mass, decreasing = TRUE),]
+sp1$MR_diff = sp1$PD_MR_VS - sp1$PD_MR_CS
+barplot(sp1$PD_MR_VS, col = "green", main = "Drosophila melanogaster")
+barplot(sp1$PD_MR_CS, col = rgb(0, 0, 0, alpha = 0.3), add = TRUE)
+abline(h = mean(sp1$PD_MR_VS), col = "green")
+abline(h = mean(sp1$PD_MR_CS))
+
+sp2 = pairs_data[pairs_data$species == "Drosophila simulans  ",]
+sp2$PD_mass = ((sp2$final_mass / sp2$initial_mass) - 1) * 100
+sp2 = sp2[order(sp2$PD_mass, decreasing = TRUE),]
+sp2$MR_diff = sp2$PD_MR_VS - sp2$PD_MR_CS
+barplot(sp2$PD_MR_VS, col = "green", main = "Drosophila simulans")
+barplot(sp2$PD_MR_CS, col = rgb(0, 0, 0, alpha = 0.3), add = TRUE)
+abline(h = mean(sp2$PD_MR_VS), col = "green")
+abline(h = mean(sp2$PD_MR_CS))
+
+sp3 = pairs_data[pairs_data$species == "Rana sylvatica",]
+sp3$PD_mass = ((sp3$final_mass / sp3$initial_mass) - 1) * 100
+sp3 = sp3[order(sp3$PD_mass, decreasing = TRUE),]
+sp3$MR_diff = sp3$PD_MR_VS - sp3$PD_MR_CS
+barplot(sp3$PD_MR_VS, col = "green", main = "Rana sylvatica")
+barplot(sp3$PD_MR_CS, col = rgb(0, 0, 0, alpha = 0.3), add = TRUE)
+abline(h = mean(sp3$PD_MR_VS), col = "green")
+abline(h = mean(sp3$PD_MR_CS))
+
+sp4 = pairs_data[pairs_data$species == "Acyrthosiphon pisum ",]
+sp4$PD_mass = ((sp4$final_mass / sp4$initial_mass) - 1) * 100
+sp4 = sp4[order(sp4$PD_mass, decreasing = TRUE),]
+sp4$MR_diff = sp4$PD_MR_VS - sp4$PD_MR_CS
+barplot(sp4$PD_MR_VS, col = "green", main = "Acyrthosiphon pisum")
+barplot(sp4$PD_MR_CS, col = rgb(0, 0, 0, alpha = 0.3), add = TRUE)
+abline(h = mean(sp4$PD_MR_VS), col = "green")
+abline(h = mean(sp4$PD_MR_CS))
+
+par(mfrow = c(2, 2))
+for (species_name in unique(sub_species_data$species)){
+  sub = filter(pairs_data, species == species_name)
+  hist(sub$PD_MR_VS, main = c(species_name), col = "green")
+  abline(v = mean(sub$PD_MR_VS), col = "green")
+  hist(sub$PD_MR_CS, col = rgb(0, 0, 0, alpha = 0.3), add = TRUE)
+  abline(v = mean(sub$PD_MR_CS))
+}
+
+par(mfrow = c(1, 2))
 bp_VS = barplot(sub_species_data$PD_MR_VS_mean)
 arrows(bp_VS, sub_species_data$PD_MR_VS_mean + sub_species_data$PD_MR_VS_sd, bp_VS, sub_species_data$PD_MR_VS_mean, angle = 90, code = 0)
 arrows(bp_VS, sub_species_data$PD_MR_VS_mean - sub_species_data$PD_MR_VS_sd, bp_VS, sub_species_data$PD_MR_VS_mean, angle = 90, code = 0)
@@ -27,29 +89,10 @@ bp_CS = barplot(sub_species_data$PD_MR_CS_mean)
 arrows(bp_CS, sub_species_data$PD_MR_CS_mean + sub_species_data$PD_MR_CS_sd, bp_CS, sub_species_data$PD_MR_CS_mean, angle = 90, code = 0)
 arrows(bp_CS, sub_species_data$PD_MR_CS_mean - sub_species_data$PD_MR_CS_sd, bp_CS, sub_species_data$PD_MR_CS_mean, angle = 90, code = 0)
 
-# PD distributions by species
-for (species_name in unique(sub_species_data$species)){
-  sub = filter(replicates_data, species == species_name)
-  #plot(density(sub$PD_MR_CS), main = c(species_name, "CS"))
-  hist(sub$PD_MR_CS, main = c(species_name, "CS"))
-  abline(v = mean(sub$PD_MR_CS))
-  #plot(density(sub$PD_MR_VS), main = c(species_name, "VS"))
-  hist(sub$PD_MR_VS, main = c(species_name, "VS"))
-  abline(v = mean(sub$PD_MR_VS))
-}
-
-# Looking at one species
-single = replicates_data[replicates_data$species == "Rana sylvatica",]
-barplot(single$PD_MR_CS, col = "white")
-barplot(single$PD_MR_VS, col = rgb(0, 0, 0, alpha = 0.3), add = TRUE)
-barplot(single$constantmass_metrate, col = rgb(0, 0, 1, alpha = 0.3))
-barplot(single$final_metrate, col = rgb(0, 1, 0, alpha = 0.3), add = TRUE)
-barplot(single$initial_metrate, col = rgb(1, 0, 0, alpha = 0.3), add = TRUE)
-
-species_data$PD_MR_diff = species_data$PD_MR_CS_mean - species_data$PD_MR_VS_mean
-species_data = species_data[order(species_data$PD_MR_diff, decreasing = TRUE),]
-
 # Barplot
+par(mfrow = c(1, 1))
+species_data$PD_MR_diff = species_data$PD_MR_CS_mean - species_data$PD_MR_VS_mean
+species_data = species_data[order(species_data$PD_MR_diff),]
 barplot(species_data$PD_MR_VS_mean, col = "green", xlab = "Species", ylab = "MR change due to temp increase")
 barplot(species_data$PD_MR_CS_mean, col = rgb(0, 0, 0, alpha = 0.3), add = TRUE)
 legend("topright", c("constant mass/no STR", "varying mass/STR"), fill = c("white", "green"))
@@ -57,18 +100,55 @@ legend("topright", c("constant mass/no STR", "varying mass/STR"), fill = c("whit
 # T-test or something? 
 #TODO: paired t-test comparing constant mass and varying mass MR PDs
 
+
 #------------------MASS ANALYSIS & VISUALIZATION---------------
 
+#TODO: change PD_mass variable name
+
 ### Mass percent difference
-replicates_data$PD_mass = ((replicates_data$constantmetrate_mass / replicates_data$initial_mass) - 1) * 100
+pairs_data$PD_mass = ((pairs_data$constantmetrate_mass / pairs_data$initial_mass) - 1) * 100
+
+### Mass differences for 1:1 plot
+pairs_data$needed_mass_diff = pairs_data$initial_mass - pairs_data$constantmetrate_mass
+pairs_data$actual_mass_diff = pairs_data$initial_mass - pairs_data$final_mass
+
+### Mass ratio for 1:1 plot
+pairs_data$needed_mass_ratio = pairs_data$constantmetrate_mass / pairs_data$initial_mass
+pairs_data$actual_mass_ratio = pairs_data$final_mass / pairs_data$initial_mass
 
 mass_comparison = replicates_data %>%
   group_by(species) %>%
   summarise_each(funs(mean), PD_mass)
 species_data$PD_mass = mass_comparison$PD_mass
 
+diffs = replicates_data %>%
+  group_by(species) %>%
+  summarise_each(funs(mean), needed_mass_diff, actual_mass_diff)
+species_data$needed_mass_diff = diffs$needed_mass_diff
+species_data$actual_mass_diff = diffs$actual_mass_diff
+
+ratios = replicates_data %>%
+  group_by(species) %>%
+  summarise_each(funs(mean), needed_mass_ratio, actual_mass_ratio)
+species_data$needed_mass_ratio = ratios$needed_mass_ratio
+species_data$actual_mass_ratio = ratios$actual_mass_ratio
+
 # Plot
 plot(density(species_data$PD_mass), main = "Needed decrease in mass", xlim = c(-40, 10))
 species_data$PD_mass_null = rnorm(45, mean = 0, sd = sd(species_data$PD_mass))
 lines(density(species_data$PD_mass_null), col = "red")
 legend("topright", c("red = null", "black = actual"))
+
+plot(species_data$needed_mass_diff, species_data$actual_mass_diff, xlim = c(-86000000, 209581910), ylim = c(-86000000, 209581910))
+lines(x = c(-86000000, 209581910), y = c(-86000000, 209581910))
+abline(h = 0, v = 0, col = "red")
+
+species_data$needed_mass_diff_log = log(species_data$needed_mass_diff)
+species_data$actual_mass_diff_log = log(species_data$actual_mass_diff)
+plot(species_data$needed_mass_diff_log, species_data$actual_mass_diff_log, xlim = c(5, 20), ylim = c(5, 20))
+lines(x = c(5, 20), y = c(5, 20))
+abline(h = 0, v = 0, col = "red")
+
+plot(species_data$needed_mass_ratio, species_data$actual_mass_ratio, xlim = c(0.6, 1.1), ylim = c(0.7, 1.35))
+lines(x = c(0.6, 1.35), y = c(0.6, 1.35))
+abline(h = 1, v = 1, col = "red", lty = 2)
